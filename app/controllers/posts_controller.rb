486beb1c94e15c486694f60c5f4c71c93d80926b
prathap442 <pmohansaikrishna@gmail.com>
2020-02-09 10:21:12 +0530

    made chagnes: like adding the comments to the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @comment = Comment.new(post_id: @post.id)
   end
 
   # GET /posts/new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def user_signed_in?
     session[:user_id] ? true : false
   end  
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      @user = User.new
+    end
+    @user
+  end    
 end
